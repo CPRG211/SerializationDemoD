7ef3296bbb95bda5765980fc7ce96e82138aefcd
Nick Hamnett <nick.hamnett@sait.ca>
2023-03-13 21:51:13 -0600

    Add project files.

---
+++ SerializationDemoD/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializationDemoD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Writing (serializing)
+            Student student1 = new Student("Joe Blow", 3.5, 21, "password");
+
+            Student student2 = new Student("John Smith", 2.3, 77, "secret");
+
+            Student student3 = new Student("Joe Schmoe", 4.0, 67, "abcd");
+
+            /*FileStream fileStream = File.OpenWrite("students.bin");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(fileStream, student1);
+            formatter.Serialize(fileStream, student2);
+            formatter.Serialize(fileStream, student3);
+
+            fileStream.Close();*/
+
+            // Reading (deserializing)
+            FileStream fileStream = File.OpenRead("students.bin");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            //for (int i = 0; i < 4; i++)
+            while (fileStream.Position < fileStream.Length)
+            {
+                object deserialized = formatter.Deserialize(fileStream);
+
+                Student student = (Student)deserialized;
+
+                Console.WriteLine("Deserialized student name: " + student.Name);
+                Console.WriteLine("Deserialized student GPA: " + student.GPA);
+            }
+            
+            /*deserialized = formatter.Deserialize(fileStream);
+
+            student = (Student)deserialized;
+
+            Console.WriteLine("Deserialized student name: " + student.Name);
+            Console.WriteLine("Deserialized student GPA: " + student.GPA);*/
+
+            fileStream.Close();
+        }
+    }
+}
---
+++ SerializationDemoD/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationDemoD
+{
+    [Serializable]
+    internal class Student
+    {
+        private string _name;
+        private double _gpa;
+
+        [OptionalField(VersionAdded = 2)]
+        private int _age;
+
+        [NonSerialized]
+        private string _password;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public double GPA
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }
+
+        
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, double gpa, int age, string password)
+        {
+            _name = name;
+            _gpa = gpa;
+            _age = age;
+            _password = password;
+        }
+    }
+}
